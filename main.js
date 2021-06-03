7ecfa0a930fa30b9985299c27dde0a509b8e41c3
SooHoonBae <jjajangmeun@gmail.com>
2021-06-03 20:43:28 +0900

    css: add trash / js: add click event

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 'use strict'
 const text = document.querySelector(".text");
 text.addEventListener('keyup', (event)=>{
-    const body = document.querySelector('body');
     const div = document.createElement('div');
-    const bottom = document.querySelector("bottom");
-    if(event.keyCode===13) {
+    const section = document.querySelector('section');
+    if(event.keyCode===13 & text.value != "") {
         div.setAttribute("class","list");
-        div.innerHTML = text.value + `   <i class="fas fa-trash-alt"></i>`;  
-        body.insertBefore(div,bottom);
+        div.innerHTML = text.value + `<i class="fas fa-trash-alt"></i>`;  
+        section.append(div);
         text.value = "";
     }    
+})
+const picon = document.querySelector('.picon');
+picon.addEventListener('click', ()=>{
+    const section = document.querySelector('section');
+    const div = document.createElement('div');
+    if(text.value != "") {
+        div.setAttribute("class","list");
+        div.innerHTML = text.value + `<i class="fas fa-trash-alt"></i>`;  
+        section.append(div);
+        text.value = "";
+    }    
+    
 })
