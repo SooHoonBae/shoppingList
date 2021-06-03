51b0096278e159b5cad306c5942823666c8d1aea
SooHoonBae <jjajangmeun@gmail.com>
2021-06-03 19:56:42 +0900

    js file/ making item enter event

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 'use strict'
 const text = document.querySelector(".text");
-text.addEventListener("keyup", (event)=>{
-    if(event.keyCode===13){
-        const name = text.value;
-        const screen = document.querySelector(".screen");
-        const result = screen.innerHTML;
-        result = name;        
-        console.log('hi');
-    };
-    
-            
-});
+text.addEventListener('keyup', (event)=>{
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+    const bottom = document.querySelector("bottom");
+    if(event.keyCode===13) {
+        div.setAttribute("class","list");
+        div.innerHTML = text.value + `   <i class="fas fa-trash-alt"></i>`;  
+        body.insertBefore(div,bottom);
+        text.value = "";
+    }    
+})
