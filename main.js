726f236e74965caccd0ff1005cdb9c3043224e38
SooHoonBae <jjajangmeun@gmail.com>
2021-06-08 17:25:35 +0900

    update js using event delegation

--- main.js
+++ main.js
@@ -14,41 +14,36 @@ function onAdd() {
     item.scrollIntoView({ block:'center'});    
     footerInput.value='';
     footerInput.focus();
-}
+};
+let id = 0;
 function createItem(text){
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class','items__item-row');
-    
-    const item = document.createElement('div');
-    item.setAttribute('class','item');
-    
-    const itemName = document.createElement('span');
-    itemName.setAttribute('class','item__name');
-    
-    const itemDelete = document.createElement('button');
-    itemDelete.setAttribute('class','item__delete');
-    itemDelete.innerHTML='<i class="fas fa-trash-alt"></i>';
-    itemDelete.addEventListener('click',()=>{
-        items.removeChild(itemRow);
-        footerInput.focus();
-    })
-
-    const divider =document.createElement('div');
-    divider.setAttribute('class','divider');
-    
-    itemRow.appendChild(item);
-    item.appendChild(itemName);
-    itemName.innerText = text;
-    
-    item.appendChild(itemDelete);
-    itemRow.appendChild(divider)
-    return itemRow;
-}
+    itemRow.setAttribute('data-id',id);    
+    itemRow.innerHTML = `
+        <div class="item">
+          <span class="item__name">${text}</span>
+          <button class="item__delete">
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+          </button>
+        </div>
+        <div class="divider"></div>
+        `;
+        id++;
+        return itemRow;
+};
 addBtn.addEventListener('click',()=>{
     onAdd();
 });
 footerInput.addEventListener('keyup',(e)=>{
     if(e.keyCode===13) {
         onAdd();
-    };
-})
+    }
+});
+items.addEventListener('click', event=> {
+    const id = event.target.dataset.id;
+    if(id) {
+        const toBeDeleted = document.querySelector(`.items__item-row[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+});
